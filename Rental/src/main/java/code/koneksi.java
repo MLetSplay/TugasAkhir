14ce602a44e01c4e1d4e7688d5f0dbb18ab62f2d
Rizr09 <rizky2004cool@gmail.com>
2022-12-14 20:59:30 +0700

    feat: login auth

---
+++ Rental/src/main/java/code/koneksi.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP VICTUS 16
+ */
+public class koneksi {
+    private String dbuser = "root";
+    private String dbpasswd = "";
+    private Statement stmt = null;
+    private Connection con = null;
+    private ResultSet rs = null;
+    public koneksi() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/parkir", dbuser, dbpasswd);
+            stmt = (Statement) con.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "" + e.getMessage(), "JDBC Driver Error ", JOptionPane.WARNING_MESSAGE); 
+        }
+    }
+    
+    public ResultSet getData(String sql) {
+        try {
+            rs = stmt.executeQuery(sql);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "" + e.getMessage(), "Communication Error", JOptionPane.WARNING_MESSAGE);
+        }
+        return rs;
+    }
+
+    public void query(String sql) {
+        try {
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "" + e.getMessage(), "Communication Error", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+}
---
+++ parkir.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2022 at 02:58 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `parkir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `userId` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`userId`, `username`, `password`) VALUES
+(1, 'rizr09', 'rizr09');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
